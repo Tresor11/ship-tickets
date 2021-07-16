20dd23b26b7e3254c3cb48ab45acef30bf23ced0
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-07-16 20:53:17 +0200

    Create NavBar component

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '../containers/Nav';
+
+const App = () => (
+  <div>
+    <NavBar />
+  </div>
+);
+
+export default App;
---
+++ src/containers/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => (
+  <div>
+    <h1>Hello Ronaldo</h1>
+  </div>
+);
+
+export default NavBar;
