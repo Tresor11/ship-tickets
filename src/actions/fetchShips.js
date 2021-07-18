bf025a194b119f322c4b09966e485724469fc24d
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-18 22:38:41 +0200

    fixup! Create the thunk for fetching the ships

--- src/actions/fetchShips.js
+++ src/actions/fetchShips.js
@@ -8,11 +8,9 @@ import {
 
 const fetchShips = () => async (dispatch) => {
   dispatch(fetchDataPending());
-  console.log('fetching ships...');
   try {
     const response = await axios.get(`${endpoint}/ships`);
     const data = await response.data;
-    console.log(data);
     dispatch(fetchDataSuccess(data));
   } catch (ex) {
     dispatch(fetchDataError(ex.message));
