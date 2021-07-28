f83a732f0c8b39da50386552991e063ccc688fb5
Tresor <tresor.moise2001@gmail.com>
2021-07-28 19:32:19 +0200

    add fetchCities method

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 
 import ships from './ships';
+import cities from './cities';
 
-const reducers = combineReducers({ ships });
+const reducers = combineReducers({ ships, cities });
 
 export default reducers;
